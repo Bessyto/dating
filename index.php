781dc3ec628146ddf1b84c6a0715a0b1f6678f7f
btorres-miller.greenriverdev.com <btorresm@srv603.webhostingforstudents.com>
2018-01-18 20:02:37 -0500

    index page added

---
+++ index.php
@@ -0,0 +1,26 @@
+<!--
+    Bessy Torres-Miller
+    home.html
+    IT-328
+    01/19/2018
+    Web site page using bootstrap
+
+-->
+
+<?php
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    $view = new View;
+    echo $view->render
+    ('pages/home.html');
+}
+);
+
+//Run fat free
+$f3->run();
